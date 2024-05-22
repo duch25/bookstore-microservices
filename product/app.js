1b75e6f58731ca77c55c5a8666dd45dd5b913e71
Duc Hoai Nguyen <nguyenduchoai25032003@gmail.com>
2024-05-23 01:17:02 +0700

    feat: implement rpc for Product Service

--- product/app.js
+++ product/app.js
@@ -16,6 +16,9 @@ const categoryRouter = require('./routes/categoryRoutes');
 
 require("dotenv").config({ path: './.env' });
 
+const { RPCObserver } = require("./rpc/rpc");
+RPCObserver(process.env.RPC_QUEUE_NAME);
+
 const app = express();
 
 app.use(cookieParser());
---
+++ product/rpc/rpc.js
@@ -0,0 +1,96 @@
+const amqplib = require('amqplib');
+const { v4: uuid4 } = require('uuid');
+
+const Book = require("../models/bookModel");
+
+let amqplibConnection = null;
+
+const getChannel = async () => {
+    if (amqplibConnection === null) {
+        amqplibConnection = await amqplib.connect(process.env.MSG_QUEUE_URL)
+    }
+
+    return await amqplibConnection.createChannel();
+}
+
+const databaseOperation = async (requestPayload) => {
+    const method = requestPayload.method;
+
+    let res = null;
+
+    if (method === 'GET') {
+        res = await Book.findOne({ isbn: requestPayload.isbn });
+    }
+
+    return res;
+}
+
+const RPCObserver = async (RPC_QUEUE_NAME) => {
+    const channel = await getChannel();
+
+    await channel.assertQueue(RPC_QUEUE_NAME, {
+        durable: false,
+    });
+
+    channel.prefetch(1);
+
+    channel.consume(
+        RPC_QUEUE_NAME,
+        async (msg) => {
+            if (msg.content) {
+                const payload = JSON.parse(msg.content.toString());
+
+                const response = await databaseOperation(payload);
+
+                channel.sendToQueue(msg.properties.replyTo,
+                    Buffer.from(JSON.stringify(response)),
+                    {
+                        correlationId: msg.properties.correlationId,
+                    }
+                );
+            }
+        },
+        {
+            noAck: true,
+        }
+    );
+}
+
+const requestData = async (RPC_QUEUE_NAME, payload, uuid) => {
+    const channel = await getChannel();
+
+    const q = await channel.assertQueue("", { exclusive: true });
+
+    channel.sendToQueue(RPC_QUEUE_NAME, Buffer.from(JSON.stringify(payload)), {
+        replyTo: q.queue,
+        correlationId: uuid
+    })
+
+    return new Promise((resolve, reject) => {
+        channel.consume(
+            q.queue,
+            (msg) => {
+                if (msg.properties.correlationId === uuid) {
+                    resolve(JSON.parse(msg.content.toString()));
+                } else {
+                    reject(undefined);
+                }
+            },
+            {
+                noAck: true,
+            }
+        )
+    })
+}
+
+const RPCRequest = async (RPC_QUEUE_NAME, payload) => {
+    const uuid = uuid4();
+
+    return await requestData(RPC_QUEUE_NAME, payload, uuid);
+}
+
+module.exports = {
+    getChannel,
+    RPCObserver,
+    RPCRequest
+}
